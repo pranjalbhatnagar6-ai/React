8b860de6f53758e1190cd37542d0e4d020472e57
pranjalbhatnagar6-ai <pranjalbhatnagar6@gmail.com>
2026-06-16 17:21:27 +0530

    Controlled components

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -335,49 +335,102 @@
 // Display Value.
 // Clear Input field Value.
 
-import { useState } from "react";
+// import { useState } from "react";
 
-function App() {
-  const [val, setVal] = useState("Pranjal Bhatnagar");
+// function App() {
+//   const [val, setVal] = useState("Pranjal Bhatnagar");
 
-  return (
-    <div>
-      <style>
-        {`
-          input::placeholder {
-            color: white;
-            opacity: 1;
-          }
-        `}
-      </style>
-      <h1>Get Input Field Value</h1>
-      <input
-        style={{
-          color: 'white',
-          margin: '10px',
-          padding: '5px',
-          backgroundColor: 'green'
-          
-        }}
+//   return (
+//     <div>
+//       <style>
+//         {`
+//           input::placeholder {
+//             color: white;
+//             opacity: 1;
+//           }
+//         `}
+//       </style>
+//       <h1>Get Input Field Value</h1>
+//       <input
+//         style={{
+//           color: 'white',
+//           margin: '10px',
+//           padding: '5px',
+//           backgroundColor: 'green'
 
-      className="input"
-      type="text"
-      value={val} //controlled input
-      onChange={(event) => setVal(event.target.value)}
-      placeholder="Enter User Name"
-       />
-      <h1>{val}</h1>
-      <button 
-      style={{
-          color: 'black',
-          margin: '10px',
-          padding: '5px',
-          borderRadius: '4px',
-      }}
-      onClick={() => setVal("")} >Clear Value</button>
-    </div>
-  );
-}
+//         }}
 
-export default App;
+//       className="input"
+//       type="text"
+//       value={val} //controlled input
+//       onChange={(event) => setVal(event.target.value)}
+//       placeholder="Enter User Name"
+//        />
+//       <h1>{val}</h1>
+//       <button 
+//       style={{
+//           color: 'black',
+//           margin: '10px',
+//           padding: '5px',
+//           borderRadius: '4px',
+//       }}
+//       onClick={() => setVal("")} >Clear Value</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 3:05:50 - # Controlled components
+
+// import { useState } from "react";
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [email, setEmail] = useState("");
+
+//   return (
+//     <div>
+//       <h1>Controlled Component</h1>
+//       <form action="" method="get">
+
+//         <input
+//           type="text"
+//           onChange={(event) => setName(event.target.value)}
+//           placeholder="Enter Name"
+//           value={name}
+//         />
+//         <br />
+//         <br />
+//         <input type="password"
+//           onChange={(event) => setPassword(event.target.value)}
+//           placeholder="Enter Password"
+//           value={password}
+//           name="password" id="password" />
+//         <br />
+//         <br />
+//         <input
+//           type="text"
+//           onChange={(event) =>
+//             setEmail(event.target.value)}
+//           value={email} />
+//         <br />
+//         <br />
+//         <button>Submit</button>
+//         <button
+//           onClick={() => {
+//             setName("");
+//             setEmail("");
+//             setPassword("");
+//           }}>Clear</button>
+//         <h1>{name}</h1>
+//         <h1>{password}</h1>
+//         <h1>{email}</h1>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
 
