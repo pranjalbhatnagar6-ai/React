c29504adabbe006bea8d450a49225da23509ee84
pranjalbhatnagar6-ai <pranjalbhatnagar6@gmail.com>
2026-06-23 23:37:54 +0530

    Learnt about how to status your form submitting by using useFormStatus

--- 01-folder/src/41_useFormStatus Hook/useFormStatusHook.jsx
+++ 01-folder/src/41_useFormStatus Hook/useFormStatusHook.jsx
@@ -1,16 +1,33 @@
-function UseFormStatus(){
-    return(
-        <>
-        <div>
-            <h1>useFormStatus Hook</h1>
-            <form action="handleSubmit">
+import { useFormStatus } from 'react-dom'
+function UseFormStatus() {
+
+    const handleSubmit = async () => {
+        await new Promise(res => setTimeout(res, 2000));
+        console.log("submit");
+    }
+
+    function CustomerForm() {
+        const { pending } = useFormStatus();
+        console.log(pending);
+        return (
+            <div>
                 <input type="text" placeholder="Enter Name" />
                 <br /><br />
-                <input type="text" placeholder="Enter Password" />
+                <input type="password" placeholder="Enter Password" />
                 <br /><br />
-                <button>Submit</button>
-            </form>
-        </div>
+                <button disabled={pending}>{pending?'Submitting...':'Submit'}</button>
+            </div>
+        ) 
+    }
+
+    return (
+        <>
+            <div>
+                <h1>useFormStatus Hook</h1>
+                <form action={handleSubmit}>
+                <CustomerForm />
+                </form>
+            </div>
         </>
     )
 }
