9b466dd0a006d57219bf28e3492ccf4cd785f4e6
pranjalbhatnagar6-ai <pranjalbhatnagar6@gmail.com>
2026-06-26 19:54:10 +0530

    learnt about fragment

---
+++ 01-folder/src/50_fragment/fragment.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+export default function Frag(){
+    return(
+        <>
+            <Data />
+        </>
+    )
+}
+
+function Data(){
+    return(
+        <div>
+            <h1>Fragment in react js</h1>
+        </div>
+    )
+}
